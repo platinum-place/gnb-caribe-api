1c1a545be18985c79395a1c793bc9b08db0e07c7
warlyn <warlyn200@gmail.com>
2025-05-11 18:25:08 -0400

    Refactor User model to remove unused imports.
    Removed unnecessary Filament, Illuminate, and Collection imports from the User model. This simplifies the code, improves readability, and removes unused dependencies.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Models\Contracts\HasTenants;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Collection;
 use Laravel\Passport\Contracts\OAuthenticatable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
