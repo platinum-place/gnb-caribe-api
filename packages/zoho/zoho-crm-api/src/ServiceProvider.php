e3fded99251f6b18622264f2f43ebd945fc351a3
warlyn <warlyn200@gmail.com>
2025-05-11 10:10:54 -0400

    Add Zoho CRM API package and update PHPUnit dependency
    Introduced a new Zoho CRM API package with Laravel integration, including service provider and facade classes. Updated PHPUnit to version 11.5.20. Adjustments made to `composer.json` and `composer.lock` to register the package and dependencies.

---
+++ packages/zoho/zoho-crm-api/src/ServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Zoho\CRM;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ packages/zoho/zoho-crm-api/src/ZohoCrm.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zoho\CRM;
+
+class ZohoCrm
+{
+
+}
